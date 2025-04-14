a4b3aa9b5c8fb20a0a5c11d37b23a1f1bd0250b6
zeeshan038 <hankhan123623@gmail.com>
2025-04-14 17:17:26 +0500

    lateest code

--- routes/orders.js
+++ routes/orders.js
@@ -2,13 +2,13 @@
 const router = require("express").Router();
 
 // controllers
-const {  test, getOrders, getSpecificOrder, orderShipped, completeOrder } = require("../controllers/Orders");
+const {  getOrders, getSpecificOrder, orderShipped, completeOrder } = require("../controllers/Orders");
 
 
 router.get("/order", getOrders);
 router.get("/specific-order/:orderId" , getSpecificOrder);
 router.put("/ship-order/:orderId" , orderShipped);
-router.put('/complete-order/:orderId',completeOrder)
+router.put('/complete-order/:orderId',completeOrder);
 
 
 module.exports = router;
